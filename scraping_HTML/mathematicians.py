93c475b6a093dce777c7d3d6aaf42b64c2244434
Behnaz Derakhshani <bderakhshani@CT-Guest.local>
2019-03-28 14:09:08 +0100

    Second commit

--- scraping_HTML/mathematicians.py
+++ scraping_HTML/mathematicians.py
@@ -38,3 +38,109 @@ def log_error(e):
     make it do anything.
     """
     print(e)
+
+from mathematicians import simple_get
+raw_html = simple_get('https://realpython.com/blog/')
+len(raw_html)
+33878
+
+no_html = simple_get('https://realpython.com/blog/nope-not-gonna-find-it')
+no_html is None
+True
+
+from bs4 import BeautifulSoup
+raw_html = open('contrived.html').read()
+html = BeautifulSoup(raw_html, 'html.parser')
+for p in html.select('p'):
+    if p['id'] == 'walrus':
+        print(p.text)
+
+raw_html = simple_get('http://www.fabpedigree.com/james/mathmen.htm')
+html = BeautifulSoup(raw_html, 'html.parser')
+for i, li in enumerate(html.select('li')):
+    print(i, li.text)
+
+def get_names():
+    """
+    Downloads the page where the list of mathematicians is found
+    and returns a list of strings, one per mathematician
+    """
+    url = 'http://www.fabpedigree.com/james/mathmen.htm'
+    response = simple_get(url)
+
+    if response is not None:
+        html = BeautifulSoup(response, 'html.parser')
+        names = set()
+        for li in html.select('li'):
+            for name in li.text.split('\n'):
+                if len(name) > 0:
+                    names.add(name.strip())
+        return list(names)
+
+    # Raise an exception if we failed to get any data from the url
+    raise Exception('Error retrieving contents at {}'.format(url))
+
+def get_hits_on_name(name):
+    """
+    Accepts a `name` of a mathematician and returns the number
+    of hits that mathematician's Wikipedia page received in the 
+    last 60 days, as an `int`
+    """
+    # url_root is a template string that is used to build a URL.
+    url_root = 'URL_REMOVED_SEE_NOTICE_AT_START_OF_ARTICLE'
+    response = simple_get(url_root.format(name))
+
+    if response is not None:
+        html = BeautifulSoup(response, 'html.parser')
+
+        hit_link = [a for a in html.select('a')
+                    if a['href'].find('latest-60') > -1]
+
+        if len(hit_link) > 0:
+            # Strip commas
+            link_text = hit_link[0].text.replace(',', '')
+            try:
+                # Convert to integer
+                return int(link_text)
+            except:
+                log_error("couldn't parse {} as an `int`".format(link_text))
+
+    log_error('No pageviews found for {}'.format(name))
+    return Noneif __name__ == '__main__':
+    print('Getting the list of names....')
+    names = get_names()
+    print('... done.\n')
+
+    results = []
+
+    print('Getting stats for each name....')
+
+    for name in names:
+        try:
+            hits = get_hits_on_name(name)
+            if hits is None:
+                hits = -1
+            results.append((hits, name))
+        except:
+            results.append((-1, name))
+            log_error('error encountered while processing '
+                      '{}, skipping'.format(name))
+
+    print('... done.\n')
+
+    results.sort()
+    results.reverse()
+
+    if len(results) > 5:
+        top_marks = results[:5]
+    else:
+        top_marks = results
+
+    print('\nThe most popular mathematicians are:\n')
+    for (mark, mathematician) in top_marks:
+        print('{} with {} pageviews'.format(mathematician, mark))
+
+    no_results = len([res for res in results if res[0] == -1])
+    print('\nBut we did not find results for '
+          '{} mathematicians on the list'.format(no_results))
+
